d58231c3ba1564df01cf6270af08693a0bbb71c7
LuffiDeMonk <119294283+LuffiDeMonk@users.noreply.github.com>
2023-07-28 17:57:54 +0545

    sixth commit

--- src/components/Reservation/Reservation.jsx
+++ src/components/Reservation/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
       <div className="absolute top-0 left-0 w-full h-full bg-black/70 grid grid-cols-1 place-content-center gap-20 lg:grid-cols-2 px-4 sm:px-16 lg:px-24 lg:gap-28 xl:px-28">
         <div
           className="text-white flex flex-col items-center lg:justify-center gap-4"
-          data-aos="fade-right"
+          data-aos="fade-up"
         >
           <h1 className="text-4xl font-semibold">
             Reserve Your Seats to Confirm if You Come with Your Family
@@ -30,7 +30,7 @@ const Reservation = () => {
             aliquip ex ea.
           </p>
         </div>
-        <div className="flex items-center justify-center" data-aos="fade-left">
+        <div className="flex items-center justify-center" data-aos="fade-up">
           <form className="h-[500px] xl:h-[540px] w-full bg-white py-24 px-8 flex flex-col gap-3 items-center xl:basis-[93%]">
             <input
               type="text"
